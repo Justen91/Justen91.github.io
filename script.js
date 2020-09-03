0096ddceaba347ec8317ace8805063fafedb68f3
Justen de Kuiper <justendekuiper@hotmail.com>
2020-09-03 16:03:34 +0100

    removing red border if incorrect value

--- script.js
+++ script.js
@@ -146,20 +146,6 @@ function isPassportNumber(number) {
 
 
 
-function validateLetters(id){
-	var element = document.getElementById(id);
-	let matchLetters = /^[A-z]{3}$/
-	textEntered =  document.getElementById(id).value 
-	if (matchLetters.test(textEntered)){
-		element.style.border = '1px solid #767676'
-
-	} else {
-		element.style.border =  '1px solid #FF0000'
-	}
-
-}
-
-
 
 function checkDigit(arrayList){
 	var outputArray = [];
@@ -246,7 +232,47 @@ function autocomplete(inp,arr){
       	}
 	});
 
-
+  /*execute a function presses a key on the keyboard:*/
+  inp.addEventListener("keydown", function(e) {
+      var x = document.getElementById(this.id + "autocomplete-list");
+      if (x) x = x.getElementsByTagName("div");
+      if (e.keyCode == 40) {
+        /*If the arrow DOWN key is pressed,
+        increase the currentFocus variable:*/
+        currentFocus++;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 38) { //up
+        /*If the arrow UP key is pressed,
+        decrease the currentFocus variable:*/
+        currentFocus--;
+        /*and and make the current item more visible:*/
+        addActive(x);
+      } else if (e.keyCode == 13) {
+        /*If the ENTER key is pressed, prevent the form from being submitted,*/
+        e.preventDefault();
+        if (currentFocus > -1) {
+          /*and simulate a click on the "active" item:*/
+          if (x) x[currentFocus].click();
+        }
+      }
+  });
+  function addActive(x) {
+    /*a function to classify an item as "active":*/
+    if (!x) return false;
+    /*start by removing the "active" class on all items:*/
+    removeActive(x);
+    if (currentFocus >= x.length) currentFocus = 0;
+    if (currentFocus < 0) currentFocus = (x.length - 1);
+    /*add class "autocomplete-active":*/
+    x[currentFocus].classList.add("autocomplete-active");
+  }
+  function removeActive(x) {
+    /*a function to remove the "active" class from all autocomplete items:*/
+    for (var i = 0; i < x.length; i++) {
+      x[i].classList.remove("autocomplete-active");
+    }
+  }
 
   function closeAllLists(elmnt) {
     var x = document.getElementsByClassName("autocomplete-items");
